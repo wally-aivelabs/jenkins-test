8d0794fb5adf175781b5ced9681c050db8a18ce8
wally <shinsanghoon@aivelabs.com>
2021-05-26 17:46:58 +0900

    delployeole

--- src/main/java/org/example/springboot/web/HelloController.java
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "안녕안녕녕하세요 웹훅 테스트 중dd...디벨롭., 포트는 8090 끙...";
+        return "안녕안녕녕하세요 웹훅 테스트 중dd...디벨롭., 포트는 8090 끙... 이제는 원격 배포 테스트 중";
     }
 }
