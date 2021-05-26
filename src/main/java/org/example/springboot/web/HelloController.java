ac1e753268427eccb5f5ecad3591d953524fcf06
wally <shinsanghoon@aivelabs.com>
2021-05-26 15:53:01 +0900

    develop commit

--- src/main/java/org/example/springboot/web/HelloController.java
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "안녕안녕녕하세요 웹훅 테스트 중";
+        return "안녕안녕녕하세요 웹훅 테스트 중...디벨롭";
     }
 
 }
