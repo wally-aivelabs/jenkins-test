13f1e3839675380a3b3e8f4975d63ecd6086b29b
wally <shinsanghoon@aivelabs.com>
2021-05-26 10:38:16 +0900

    test

--- src/main/java/org/example/springboot/web/HelloController.java
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello-jenkins";
+        return "hello-jenkins~~~~~";
     }
 
 }
